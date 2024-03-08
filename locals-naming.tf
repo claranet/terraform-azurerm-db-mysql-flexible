27abb1f806036562371f304b051870620ae341db
Remi Beaufils <remi.beaufils@fr.clara.net>
2024-03-08 11:28:40 +0100

    AZ-1356: Fix name_prefix in naming

--- locals-naming.tf
+++ locals-naming.tf
@@ -1,5 +1,5 @@
 locals {
-  name_prefix = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
+  name_prefix = lower(var.name_prefix)
   name_suffix = lower(var.name_suffix)
 
   mysql_flexible_server_name = coalesce(var.custom_server_name, lower(data.azurecaf_name.mysql_flexible_name.result))
